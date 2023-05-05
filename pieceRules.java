e2b8e3889487d0a98895867dccfb6015f3bfefe7
Snare-Hawk <85657152+Snare-Hawk@users.noreply.github.com>
2023-05-05 14:26:45 -0400

    i think this is what you had in mind

---
+++ pieceRules.java
@@ -0,0 +1,25 @@
+public class pieceRules {
+    public class pawn extends pieces {
+
+    }
+
+    public class rook extends pieces {
+
+    }
+
+    public class knight extends pieces {
+
+    }
+
+    public class bishop extends pieces {
+
+    }
+
+    public class king extends pieces {
+
+    }
+
+    public class queen extends pieces {
+
+    }
+}
---
+++ pieces.java
@@ -0,0 +1,25 @@
+public class pieces extends pieceRules {
+    public class pawn {
+
+    }
+
+    public class rook {
+
+    }
+
+    public class knight {
+
+    }
+
+    public class bishop {
+
+    }
+
+    public class king {
+
+    }
+
+    public class queen {
+
+    }
+}
